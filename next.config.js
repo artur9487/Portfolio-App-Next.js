27bf8d0b212978c6b38ae8d81eec36d020971710
artur9487 <artur.www9487@gmail.com>
2022-08-11 00:03:39 +0200

    Commit

--- next.config.js
+++ next.config.js
@@ -8,12 +8,7 @@ const nextConfig = {
 };
 
 module.exports = {
-	env: {
-		API: 'cl1b113lg0pta01z32tyx7jjg',
-		cont: 'service_d6rbqns',
-		cont2: 'template_3umjr64',
-		cont3: '27Y9n12lnaZfUOWyE'
-	},
+	env: {},
 	nextConfig,
 	images: {
 		domains: ['media.graphcms.com', 'media.graphassets.com']
